c429db1c4a5127f5b6c9979de50c9c3b0cac89a9
openmcse <openmcse@lgupluspartners.co.krgit config --global user.name git commit -m >
2021-01-02 00:26:30 +0900

    환경세팅 완료

---
+++ src/test/java/com/marine/blog/BlogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.marine.blog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
